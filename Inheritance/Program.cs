8bd1a5a692a82ee002fe5775976774e82e89bc3b
user <user@DESKTOP-7EH2G6F>
2022-02-18 09:30:21 +0530

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Employee
+    {
+        public string EmpName;
+        public int EmpID;
+        public int Salary;
+
+
+    }
+    class VistingEmployee : Employee
+    {
+       
+    }
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            VistingEmployee Sonal = new VistingEmployee();
+                Sonal.EmpID = 1456;
+            Sonal.Salary = 50000;
+                Sonal.EmpName = "Sonal";
+
+            Console.WriteLine("Sonal Employee ID is:{0}", Sonal.EmpID);
+            Console.WriteLine("Sonal Salary is:{0}", Sonal.Salary);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
